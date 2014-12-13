438fd9e8aa3a6f534611c1c88c1aec55412e4f8d
MebiNekophus <gappoi007@gmail.com>
2014-12-13 20:04:59 +0900

    にゃにゃ
    てすと

--- src/com/nekophus/mebitwitter/MainActivity.java
+++ src/com/nekophus/mebitwitter/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends ListActivity {
 
@@ -24,6 +25,12 @@ public class MainActivity extends ListActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    public class TwitterAdapter extends ArrayAdapter<String> {
+    	public TweetAdapter(Context context) {
+    		super(context, android.R.layout.simple_list_item_1);
+    	}
     }
 }
 
